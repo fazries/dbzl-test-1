ae4b6f118ddb9da324d9f7db2d0859a32a76225a
fazrie <fazrie@kitabisa.com>
2019-09-27 08:09:52 +0700

    added master host

--- puppet/install.sh
+++ puppet/install.sh
@@ -32,7 +32,10 @@ if [ "$1" != "" ] ; then
 	  git clone https://github.com/fazries/dbzl-test-1.git
 	  cd dbzl-test-1/
           sudo cp puppet/client/puppet.conf /etc/puppet/
-
+        
+        add)
+	  #add master ip address and domain
+	  echo "$2  master.dbzl.test" | sudo tee -a /etc/hosts
 
           ;;		
         *)
